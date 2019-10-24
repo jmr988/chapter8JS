5b14062b873a28835b29da0cb093fd1e1529aebc
jmr988 <jmrodriguez98842@gmail.com>
2019-10-24 13:56:41 -0500

    step 5

--- HandsOnProject8-3/script.js
+++ HandsOnProject8-3/script.js
@@ -5,3 +5,12 @@ Author: Jose Rodriguez
 script.js
 */
 'use strict';
+
+function selectCardType(){
+	
+	var cardNumValue = document.getElementById("ccNum").value;
+	var visa = /^4[0-9]{12}(?:[0-9]{3}?$/;
+	var mc = /^5[1-5][0-9]{14}$/;
+	var discover = /^6(?:011|5[0-9]{2})[0-9]{12}$/;
+	var amex = /^3[47][0-9]{13}$/;
+}
