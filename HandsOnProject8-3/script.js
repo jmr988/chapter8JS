2fa3344be10582a609de5d02b265a54ee17a5668
jmr988 <33246637+jmr988@users.noreply.github.com>
2019-10-24 21:09:44 -0500

    Update script.js

--- HandsOnProject8-3/script.js
+++ HandsOnProject8-3/script.js
@@ -4,7 +4,7 @@ Author: Jose Rodriguez
 10/24/2019
 script.js
 */
-'use strict';
+"use strict";
 
 function selectCardType(){
 	
@@ -13,4 +13,28 @@ function selectCardType(){
 	var mc = /^5[1-5][0-9]{14}$/;
 	var discover = /^6(?:011|5[0-9]{2})[0-9]{12}$/;
 	var amex = /^3[47][0-9]{13}$/;
+	
+	if (visa.test(cardNumValue)) {
+		document.getElementById ("visa").checked = "checked";
+	} else if (mc.test(cardNumValue)) {
+		document.getElementById("mc").checked = "checked";
+	} else if (discover.test(cardNumValue)) {
+		document.getElementById("discover").checked ="checked";
+	} else if (amex.test(cardNumValue)) {
+		document.getElementById("amex").checked = "checked";
+	}
+}
+
+function createEventListeners(){
+	var cardNum = document.getElementById("ccNum");
+	if (cardNum.addEventListener) {
+		cardNum.addEventListener("change", selectCardType, false);
+	} else if (cardNum.attachEvent) {
+		cardNum.attachEvent("onchange", selectCardType);
+	}
+}
+if (window.addEventListener) {
+	window.addEventListener("load", createEventListeners , false);
+} else if (window.attachEvent) {
+	window.attachEvent("onload", createEventListeners);
 }
